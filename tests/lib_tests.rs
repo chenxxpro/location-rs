2af41713a612cd247c6edd9ffd5b0167deaa43f2
chenxxpro <chenxxpro@hotmail.com>
2025-10-22 20:49:05 +0800

    test: 更新测试用例以匹配新的国家代码枚举值

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -1,27 +1,28 @@
-use location_rs::{parse_country_code, CountryCode, ParseError};
+use location_rs::{parse_country_code, CountryCode};
 
 #[test]
 fn test_basic_iso_code_parsing() {
     // 测试基本的ISO代码解析
-    assert_eq!(parse_country_code("US Node").unwrap(), CountryCode::US);
-    assert_eq!(parse_country_code("CN Server").unwrap(), CountryCode::CN);
-    assert_eq!(parse_country_code("HK Vip").unwrap(), CountryCode::HK);
+    assert_eq!(parse_country_code("US Node").unwrap(), CountryCode::USA);
+    assert_eq!(parse_country_code("CN Server").unwrap(), CountryCode::CHN);
+    assert_eq!(parse_country_code("HK Vip").unwrap(), CountryCode::HKG);
 }
 
 #[test]
 fn test_chinese_name_parsing() {
     // 测试中文名称解析
-    assert_eq!(parse_country_code("美国节点").unwrap(), CountryCode::US);
-    assert_eq!(parse_country_code("中国服务器").unwrap(), CountryCode::CN);
-    assert_eq!(parse_country_code("香港服务").unwrap(), CountryCode::HK);
+    assert_eq!(parse_country_code("美国节点").unwrap(), CountryCode::USA);
+    assert_eq!(parse_country_code("中国服务器").unwrap(), CountryCode::CHN);
+    assert_eq!(parse_country_code("香港服务").unwrap(), CountryCode::HKG);
 }
 
 #[test]
 fn test_mixed_patterns() {
     // 测试混合模式
-    assert_eq!(parse_country_code("@HK Vip1").unwrap(), CountryCode::HK);
-    assert_eq!(parse_country_code("【SS】US1").unwrap(), CountryCode::US);
-    assert_eq!(parse_country_code("V1 美国").unwrap(), CountryCode::US);
+    assert_eq!(parse_country_code("@HK Vip1").unwrap(), CountryCode::HKG);
+    assert_eq!(parse_country_code("【SS】USA1").unwrap(), CountryCode::USA);
+    assert_eq!(parse_country_code("【SS】US1").unwrap(), CountryCode::USA);
+    assert_eq!(parse_country_code("V1 美国").unwrap(), CountryCode::USA);
 }
 
 #[test]
@@ -35,14 +36,14 @@ fn test_error_cases() {
 #[test]
 fn test_case_insensitive() {
     // 测试大小写不敏感
-    assert_eq!(parse_country_code("us node").unwrap(), CountryCode::US);
-    assert_eq!(parse_country_code("cn server").unwrap(), CountryCode::CN);
+    assert_eq!(parse_country_code("us node").unwrap(), CountryCode::USA);
+    assert_eq!(parse_country_code("cn server").unwrap(), CountryCode::CHN);
 }
 
 #[test]
 fn test_edge_cases() {
     // 测试边界情况
-    assert_eq!(parse_country_code("US").unwrap(), CountryCode::US);
-    assert_eq!(parse_country_code("美国").unwrap(), CountryCode::US);
-    assert_eq!(parse_country_code("United States").unwrap(), CountryCode::US);
+    assert_eq!(parse_country_code("US").unwrap(), CountryCode::USA);
+    assert_eq!(parse_country_code("美国").unwrap(), CountryCode::USA);
+    assert_eq!(parse_country_code("United States").unwrap(), CountryCode::USA);
 }
